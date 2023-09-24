ee18074e2a3ebb552c8058f1ce9e60da554b88d5
Kwame Akuamoah-Boateng <kaboateng14@gmail.com>
2023-09-24 21:24:39 +0000

    Refactor code to make it more modular

--- main.go
+++ main.go
@@ -2,21 +2,34 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/quamejnr/heimdall/util"
 	"os"
 )
 
 func main() {
+	// Get command line args
+	cmd := flag.String("cmd", "", "Shell command to be run")
+	fileName := flag.String("f", "", "Name of file to be searched for")
+	strict := flag.Bool("s", false, "Strict name matching. If `true` filename/folder provided should be the exact match.")
+	flag.Parse()
+
 	// Get into the user documents directory
 	root, _ := os.UserHomeDir()
 	os.Chdir(root + "/Documents")
 
-	// Get command line args
-	cmd := flag.String("cmd", "", "Shell command to be run")
-	file := flag.String("f", "", "Name of file to be searched for")
-	strict := flag.Bool("s", false, "Strict name matching. If `true` filename/folder provided should be the exact match.")
-	flag.Parse()
+	files := util.FindFiles(*fileName, *strict)
+	var f string
 
-	files := util.FindFiles(*file, *strict)
-	util.RunCommand(*cmd, files)
+	switch numFiles := len(files); {
+	case numFiles == 0:
+		fmt.Println("File Not Found")
+		return
+	case numFiles == 1:
+		f = files[0]
+	default:
+		f = util.PickFile(files)
+	}
+	util.RunCommand(*cmd, f)
+
 }
--- util/util.go
+++ util/util.go
@@ -9,45 +9,7 @@ import (
 	"strings"
 )
 
-// RunCommand runs shell commands provided by the parameter `command` on the string files.
-func RunCommand(command string, files []string) {
-	switch numFiles := len(files); {
-	case numFiles == 0:
-		fmt.Println("File not found")
-		return
-
-	case numFiles == 1:
-		cmd := exec.Command(command, files[0])
-		cmd.Stdin = os.Stdout
-		cmd.Stdout = os.Stdout
-		if err := cmd.Run(); err != nil {
-			fmt.Println("ERROR: Command couldn't run on file.\nCommand may be running on wrong file or program does not support.", command)
-			return
-		}
-
-	default:
-		fmt.Println("Choose option: ")
-		for i, f := range files {
-			fmt.Printf("(%d)\t%s\n", i+1, f)
-		}
-
-		var input int
-		fmt.Scanln(&input)
-
-		if input > len(files) || input < 0 {
-			fmt.Printf("Invalid option: '%d'. Choose between range 1 - %d\n", input, len(files))
-			RunCommand(command, files)
-			return
-		}
-		// Put the chosen file into a slice to be passed to RunCommand function
-		files := []string{files[input-1]}
-		RunCommand(command, files)
-		return
-	}
-
-}
-
-// FindFiles returns list of files and folders whose name matches the string fname. 
+// FindFiles returns list of files and folders whose name matches the string fname.
 // When the parameter strict is true, it returns files/folders with the exact match as fname.
 // If strict is false, it matches files/folders irrespective of case and extension.
 func FindFiles(fname string, strict bool) []string {
@@ -70,6 +32,37 @@ func FindFiles(fname string, strict bool) []string {
 	if err != nil {
 		return result
 	}
-
 	return result
+}
+
+// PickFile returns name of a file/folder among a list of files/folders.
+// Returns the only name of the file/folder if only one file is returned
+func PickFile(files []string) string {
+	if len(files) == 1 {
+		return files[0]
+	}
+	fmt.Println("Choose option: ")
+	for i, f := range files {
+		fmt.Printf("(%d)\t%s\n", i+1, f)
+	}
+
+	var input int
+	fmt.Scanln(&input)
+
+	for input > len(files) || input < 0 {
+		fmt.Printf("Invalid option: '%d'. Choose between range 1 - %d\n", input, len(files))
+	}
+	return files[input-1]
+}
+
+// RunCommand runs shell commands provided by the parameter `command` on a file.
+func RunCommand(command string, file string) {
+	cmd := exec.Command(command, file)
+	cmd.Stdin = os.Stdout
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		fmt.Println("ERROR: Command couldn't run on file.\nCommand may be running on wrong file or program does not support.", command)
+		return
+	}
+
 }
