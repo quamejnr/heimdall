6cf3251e7ef141f37f9f3a040cfb9a686bc56acb
Kwame Akuamoah-Boateng <kaboateng14@gmail.com>
2023-09-25 21:50:44 +0000

    Implemented cobra in project

---
+++ cmd/root.go
@@ -0,0 +1,72 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	// "flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/quamejnr/heimdall/utils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	fileName, command string
+	strict bool
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "heimdall",
+	Short: "Heimdall is a CLI tool that allows you to run your basic shell commands on a file/folder from anywhere.",
+	Long: `
+Heimdall is a CLI tool that allows you to run your basic shell commands like 'cat', 'code', 'ls', 'vim' etc. on a file 
+The beauty about Heimdall is it allows you to run these commands on your file/folder without having to navigate to the directory your file/folder exists in. 
+For example: 'heimdall vim example.go' or 'heimdall -c=vim -f=example.go' will open your file 'example.go' in vim. 
+
+NB: File/Folder needs to be in '$USER/Documents' directory.
+`,
+	
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 2 {
+			command, fileName = args[0], args[1]
+		}
+		heimdall(command, fileName, strict)
+	 },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&command, "cmd", "c", "", "Shell command")
+	rootCmd.PersistentFlags().StringVarP(&fileName, "filename", "f", "", "File/Folder to run shell command on")
+	rootCmd.Flags().BoolVarP(&strict, "strict", "s", true, "Strict name matching")
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func heimdall(command string, fileName string, strict bool) {
+	// Get into the user documents directory
+	root, _ := os.UserHomeDir()
+	dir := filepath.Join(root, "Documents")
+	os.Chdir(dir)
+
+	files := utils.FindFiles(fileName, strict)
+	if len(files) == 0 {
+		fmt.Printf("'%s' not Found\n", fileName)
+		return 
+	}
+	f := utils.PickFile(files)
+	utils.RunCommand(command, f)
+
+}
--- main.go
+++ main.go
@@ -1,32 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-	"path/filepath"
-
-	"github.com/quamejnr/heimdall/util"
-)
+import "github.com/quamejnr/heimdall/cmd"
 
 func main() {
-	// Get command line args
-	cmd := flag.String("cmd", "", "Shell command to be run")
-	fileName := flag.String("f", "", "Name of file to be searched for")
-	strict := flag.Bool("s", true, "Strict name matching. If `true` filename/folder provided should be the exact match.")
-	flag.Parse()
-
-	// Get into the user documents directory
-	root, _ := os.UserHomeDir()
-	dir := filepath.Join(root, "Documents")
-	os.Chdir(dir)
-
-	files := util.FindFiles(*fileName, *strict)
-	if len(files) == 0 {
-		fmt.Printf("'%s' not Found\n", *fileName)
-		return 
-	}
-	f := util.PickFile(files)
-	util.RunCommand(*cmd, f)
-
+	cmd.Execute()
 }
